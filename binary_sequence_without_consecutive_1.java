5ac7c1520fbc9380914ed50857fed9aaa99a59d4
anindyasarkarIITH <anindyasarkar.ece@gmail.com>
2016-06-23 00:47:12 +0530

    Add files via upload

---
+++ binary_sequence_without_consecutive_1.java
@@ -0,0 +1,24 @@
+package bucky;
+
+public class DP {
+	public int fib(int n){
+		if(n==1){
+			return 0;
+		}
+		else if(n==2){
+			return 1;
+		}
+		else{
+			return (fib(n-1)+fib(n-2));
+		}
+	}
+	public int Count(int n){
+		return fib(n+3);
+	}
+	public static void main(String[] args){
+		int[] input={8,10,10,2};
+		DP dp=new DP();
+		int t=dp.Count(6);
+		System.out.println(t);
+	}
+}
